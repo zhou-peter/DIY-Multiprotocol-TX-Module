f7ef43077c957b08b56f950b704c437784f48954
mikeller <github@ike.ch>
2016-10-20 23:32:48 +1300

    Config.
    Removed some protocols to make it fit.
    
    Changed settings for 1.2.0.
    
    Removed some more protocols for 1.3.1.
    
    Moved changes into MyConfig.h.

---
+++ Multiprotocol/_MyConfig.h
@@ -0,0 +1,88 @@
+/*
+   This file is meant to keep your settings after an upgrade of the multi source.
+   If you know parameters you want for sure to be enabled or disabled in future
+   then just force their values here.
+   To enable  a setting use #define <setting name>
+   To disable a setting use #undef  <setting name>
+*/
+
+#define CHECK_FOR_BOOTLOADER
+
+#define FORCE_GLOBAL_ID 0x95e6452e
+#define FORCE_CYRF_ID "\x95\xe6\x45\x2e\x9d\xfd"
+
+#undef ENABLE_BIND_CH
+
+#undef ENABLE_PPM
+
+#undef      AFHDS2A_A7105_INO
+#undef      AFHDS2A_RX_A7105_INO
+#undef      BUGS_A7105_INO
+
+#undef      HEIGHT_A7105_INO
+#undef      HUBSAN_A7105_INO
+#undef      KYOSHO_A7105_INO
+#undef      PELIKAN_A7105_INO
+
+#undef      DEVO_CYRF6936_INO
+
+#undef      J6PRO_CYRF6936_INO
+#undef      TRAXXAS_CYRF6936_INO
+#undef      WFLY_CYRF6936_INO
+#undef      WK2x01_CYRF6936_INO
+
+#undef      CORONA_CC2500_INO
+#undef      ESKY150V2_CC2500_INO
+
+#undef      HITEC_CC2500_INO
+#undef      HOTT_CC2500_INO
+#undef      OMP_CC2500_INO
+#undef      SCANNER_CC2500_INO
+
+#undef      SKYARTEC_CC2500_INO
+
+#undef      RLINK_CC2500_INO
+
+#undef      ASSAN_NRF24L01_INO
+#undef      BAYANG_NRF24L01_INO
+#undef      BAYANG_RX_NRF24L01_INO
+#undef      BUGSMINI_NRF24L01_INO
+#undef      CABELL_NRF24L01_INO
+#undef      CFLIE_NRF24L01_INO
+#undef      CG023_NRF24L01_INO
+#undef      CX10_NRF24L01_INO
+#undef      DM002_NRF24L01_INO
+#undef      E01X_NRF24L01_INO
+#undef      ESKY_NRF24L01_INO
+#undef      ESKY150_NRF24L01_INO
+
+#undef      FX816_NRF24L01_INO
+#undef      FY326_NRF24L01_INO
+#undef      GD00X_NRF24L01_INO
+#undef      GW008_NRF24L01_INO
+#undef      HISKY_NRF24L01_INO
+#undef      HONTAI_NRF24L01_INO
+#undef      H8_3D_NRF24L01_INO
+#undef      JJRC345_NRF24L01_INO
+#undef      KF606_NRF24L01_INO
+#undef      KN_NRF24L01_INO
+#undef      MJXQ_NRF24L01_INO
+#undef      MT99XX_NRF24L01_INO
+#undef      NCC1701_NRF24L01_INO
+#undef      POTENSIC_NRF24L01_INO
+#undef      PROPEL_NRF24L01_INO
+#undef      Q303_NRF24L01_INO
+#undef      Q90C_NRF24L01_INO
+#undef      REALACC_NRF24L01_INO
+#undef      SHENQI_NRF24L01_INO
+#undef      SLT_NRF24L01_INO
+#undef      SYMAX_NRF24L01_INO
+#undef      TIGER_NRF24L01_INO
+#undef      V2X2_NRF24L01_INO
+#undef      V761_NRF24L01_INO
+#undef      V911S_NRF24L01_INO
+#undef      XK_NRF24L01_INO
+#undef      YD717_NRF24L01_INO
+#undef      ZSX_NRF24L01_INO
+
+#undef      FRSKYR9_SX1276_INO
